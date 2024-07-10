d81959cea03c18d228087b338e5bc933cc1af14d
Tejash Maurya <mauryatejash00@gmail.com>
2024-07-10 20:43:33 +0530

    thank y

---
+++ src/thank-you/page.tsx
@@ -0,0 +1,26 @@
+// import { Suspense } from "react"
+// import ThankYou from "./ThankYou"
+
+// const Page=()=>{
+//     return(
+//         <Suspense>
+//             <ThankYou />
+//         </Suspense>
+//     )
+// }
+
+// export default Page
+
+
+import { Suspense } from 'react'
+import ThankYou from './ThankYou'
+
+const Page = () => {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  )
+}
+
+export default Page
